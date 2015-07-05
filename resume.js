e738386791573c7e29230a8c2a93e2a78139455f
José Pedro Dias <jose.pedro.dias@gmail.com>
2015-07-05 13:01:06 +0100

    v1

--- resume.js
+++ resume.js
@@ -2,20 +2,55 @@ var fs = require('fs');
 
 var song = JSON.parse( fs.readFileSync('out.json').toString() );
 
-var measures = song['score-partwise']['part'].measure;
+/**
+ * % object index
+ * $ attributes hash
+ * _ text
+ */
+
+var scorePartwise = song['score-partwise'];
+
+
+
+// text
+scorePartwise.credit.forEach(function(credit) {
+	console.log('TEXT: ' + credit['credit-words']._);
+});
+
+
+
+// instruments
+var sp = scorePartwise['part-list']['score-part'];
+var parseSP = function(sp) {
+	console.log('CHANNEL:')
+	console.log('  name:    ' + sp['part-name']);
+	var mi = sp['midi-instrument'];
+	console.log('  channel: ' + mi['midi-channel']);
+	console.log('  program: ' + mi['midi-program']);
+	console.log('  volume:  ' + mi['volume']);
+	console.log('  pan:     ' + mi['pan']);
+};
+parseSP(sp);
+
+
+
+// measures w/ notes
+var measures = scorePartwise['part'].measure;
 
 measures.forEach(function(measure, mi) {
+
 	var attrs = measure.attributes;
-	console.log('measure', mi);
+	console.log('MEASURE' + mi);
 	if (attrs && 'time' in attrs) {
-		console.log(attrs.time.beats, attrs.time['beat-type']);
+		console.log('  time: ' + attrs.time.beats + ' / ' + attrs.time['beat-type']);
 	}
+
 	measure.note.forEach(function(note, ni) {
 		if (note.rest) {
-			console.log(ni, note.voice, note['%'], note.duration, 'rest');
+			console.log(['  vc:', note.voice, ', dur:', note.duration, ' rest'].join(''));
 		}
 		else {
-			console.log(ni, note.voice, note['%'], note.duration, note.type, note.pitch.step, note.pitch.octave);
+			console.log(['  vc:', note.voice, ', dur:', note.duration, ' ', note.type, ' ', note.pitch.step, note.pitch.octave].join(''));
 		}
 	});
 });
